7f536b1adf3bfeea33704417a7c04d2c7e2366a3
John Le Drew <john@rainbowlaces.com>
2026-06-26 12:32:19 +0200

    feat(cmd): register LawGroupReconciler in controller manager

--- platform/operator/cmd/main.go
+++ platform/operator/cmd/main.go
@@ -265,6 +265,13 @@ func main() {
 		setupLog.Error(err, "Failed to create controller", "controller", "CodificationService")
 		os.Exit(1)
 	}
+	if err := (&controller.LawGroupReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "Failed to create controller", "controller", "LawGroup")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
